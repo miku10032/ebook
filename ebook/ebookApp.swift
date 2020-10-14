3cd04f1dda78abfffb056bcd2842fc321175438b
jubao <tqy_4896@hotmail.com>
2020-10-14 10:25:12 +0800

    Initial Commit

---
+++ ebook/ebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ebookApp.swift
+//  ebook
+//
+//  Created by User16 on 2020/10/14.
+//
+
+import SwiftUI
+
+@main
+struct ebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
